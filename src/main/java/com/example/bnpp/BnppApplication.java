2d31e8ed2852fda089055919ba41f5a6a7727cb5
hmidiala <hmidialaaeddine@gmail.com>
2022-11-17 23:34:10 +0100

    initial commit

---
+++ src/main/java/com/example/bnpp/BnppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bnpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bnpp/BnppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bnpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
